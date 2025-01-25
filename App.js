c5256362a933d092fe6eb4370cf7687c0d250b08
Poonam Awajare <poonamawajare@gmail.com>
2025-01-25 13:24:10 +0530

    This the simple practice project and well not designed

---
+++ App.js
@@ -0,0 +1,23 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "Delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value="";
+})
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
